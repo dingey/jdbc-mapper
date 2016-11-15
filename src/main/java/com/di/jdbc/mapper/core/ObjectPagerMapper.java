fd5cf6fe2ae7ef09f24fd29432a1083e48c2a5a9
ding288 <123929187@qq.com>
2016-11-15 10:29:48 +0800

    prepared named pager

--- src/main/java/com/di/jdbc/mapper/core/ObjectPagerMapper.java
+++ src/main/java/com/di/jdbc/mapper/core/ObjectPagerMapper.java
@@ -1,5 +1,7 @@
 package com.di.jdbc.mapper.core;
 
+import com.di.jdbc.mapper.annotation.NamedNativeQueries;
+import com.di.jdbc.mapper.annotation.NamedNativeQuery;
 import com.di.jdbc.mapper.util.ExampleUtil;
 import com.di.jdbc.mapper.util.Pager;
 import com.di.jdbc.mapper.util.PagerSqlUtil;
@@ -33,6 +35,31 @@ public class ObjectPagerMapper extends ObjectExampleMapper {
 		p.setPageSize(pageSize);
 		p.setTotal(this.prepareQueryForSingleValue(sql0, args, long.class));
 		p.setList(prepareQueryForList(PagerSqlUtil.getPreparePageSql(preSql, pageNum, pageSize, fileName), args, t));
+		return p;
+	}
+
+	public <T> Pager<T> prepareNamedQueryPager(String queryName, Object[] args, int pageNum, int pageSize, Class<T> t) {
+		Pager<T> p = new Pager<>();
+		String preSql = "";
+		if (t.isAnnotationPresent(NamedNativeQueries.class) || t.isAnnotationPresent(NamedNativeQuery.class)) {
+			if (t.getAnnotation(NamedNativeQuery.class) != null
+					&& t.getAnnotation(NamedNativeQuery.class).name().equals(queryName)) {
+				preSql = t.getAnnotation(NamedNativeQuery.class).query();
+			} else {
+				for (NamedNativeQuery nq : t.getAnnotation(NamedNativeQueries.class).value()) {
+					if (nq.name().equals(queryName)) {
+						preSql = nq.query();
+						break;
+					}
+				}
+			}
+		}
+		String sql0 = "select count(0) " + preSql.substring(preSql.indexOf("from"));
+		p.setPageNum(pageNum);
+		p.setPageSize(pageSize);
+		p.setTotal(this.prepareQueryForSingleValue(sql0, args, long.class));
+		p.setList(
+				prepareQueryForList(PagerSqlUtil.getPreparePageSql(preSql, pageNum, pageSize, getFileName()), args, t));
 		return p;
 	}
 
