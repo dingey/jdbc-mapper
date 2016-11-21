8b5b668edcff259b1a6987a1c0bcece296668b06
ding288 <123929187@qq.com>
2016-11-21 09:56:01 +0800

    edit annotation

--- src/main/java/com/di/jdbc/mapper/annotation/Table.java
+++ src/main/java/com/di/jdbc/mapper/annotation/Table.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 /**
  * @author di
  */
-@Target({ java.lang.annotation.ElementType.TYPE })
+@Target({ java.lang.annotation.ElementType.TYPE, java.lang.annotation.ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Table {
 	public abstract String name();
